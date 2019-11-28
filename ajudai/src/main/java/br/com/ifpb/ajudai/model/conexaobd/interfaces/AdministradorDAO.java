3834d05c8100fc0ab111bf9bf1331ec76ef8a918
James Amarante <jamesamarante13@gmail.com>
2019-11-27 23:46:17 -0300

    Remvendo arquivos

--- ajudai/src/main/java/br/com/ifpb/ajudai/model/conexaobd/interfaces/AdministradorDAO.java
+++
@@ -1,9 +0,0 @@
-package br.com.ifpb.ajudai.model.conexaobd.interfaces;
-
-import br.com.ifpb.ajudai.model.entidades.Administrador;
-
-import java.sql.SQLException;
-
-public interface AdministradorDAO {
-    boolean atualizarAdministrador(Administrador administrador) throws SQLException, ClassNotFoundException;
-}
