d0cb9d4a1c27c40069bbdf7702a234c42effe6e7
LuzEsperanza <github.com/LuzEsperanza>
2019-12-04 17:34:52 -0300

    adcionando scripts

--- ajudai/src/main/resources/BaseDados/scripts.sql
+++ ajudai/src/main/resources/BaseDados/scripts.sql
@@ -1,20 +1,14 @@
-﻿CREATE TABLE Usuario(
+﻿﻿CREATE TABLE Usuario(
 	Codigo SERIAL,
 	NomeCompleto VARCHAR(200) NOT NULL,
     NomeUsuario VARCHAR(200) NOT NULL,
 	DataNascimento DATE NOT NULL,
 	Email VARCHAR(100) NOT NULL,
 	Senha VARCHAR(100) NOT NULL,
+	Ativo BOOLEAN DEFAULT TRUE NOT NULL,
 	CONSTRAINT PK_Usuario PRIMARY KEY(Codigo)
 );
 
-CREATE TABLE TelefoneUsuario(
-	CodUsuario INTEGER,
-	Telefone VARCHAR(20),
-	CONSTRAINT PK_TelefoneUsuario PRIMARY KEY(CodUsuario, Telefone),
-	CONSTRAINT FK_TelegoneUsuario FOREIGN KEY(CodUsuario) REFERENCES Usuario(Codigo) 
-		ON UPDATE CASCADE ON DELETE RESTRICT
-); 
 
 CREATE TABLE Especialista(
 	CodUsuario INTEGER,
@@ -29,18 +23,20 @@ CREATE TABLE AreaEspecialista(
 	CodEspecialista INTEGER,
 	Area VARCHAR(50),
 	CONSTRAINT PK_AreaEspecialista PRIMARY KEY(CodEspecialista, Area),
-	CONSTRAINT FK_AreaEspecialista FOREIGN KEY(CodEspecialista) REFERENCES Especialista(CodUsuario) 
+	CONSTRAINT FK_AreaEspecialista FOREIGN KEY(CodEspecialista) REFERENCES Especialista(CodUsuario)
 		ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
 
 CREATE TABLE Administrador(
-	CodUsuario INTEGER,
+	Codigo INTEGER,
+	NomeCompleto VARCHAR(200) NOT NULL,
+	Email VARCHAR(100) NOT NULL,
+	Senha VARCHAR(100) NOT NULL,
 	DataInicio DATE NOT NULL,
 	DataFim DATE NOT NULL,
-	CONSTRAINT PK_Administrador PRIMARY KEY(CodUsuario),
-	CONSTRAINT FK_Administrador FOREIGN KEY(CodUsuario) REFERENCES Usuario(Codigo)
-		ON UPDATE CASCADE ON DELETE RESTRICT
+	CONSTRAINT PK_Administrador PRIMARY KEY(Codigo)
+
 );
 
 CREATE TABLE Postagem(
@@ -49,36 +45,39 @@ CREATE TABLE Postagem(
 	Descricao VARCHAR(500) NOT NULL,
 	DataPostagem DATE NOT NULL,
 	CodUsuario INTEGER,
+	Denuncia INTEGER,
 	CONSTRAINT PK_Postagem PRIMARY KEY(Id),
 	CONSTRAINT FK_Postagem FOREIGN KEY(CodUsuario) REFERENCES Usuario(Codigo)
 		ON UPDATE CASCADE ON DELETE RESTRICT
 );
-
 
 CREATE TABLE Comentario(
 	Codigo SERIAL,
 	IdPostagem INTEGER,
     Descricao VARCHAR(500) NOT NULL,
 	DataComentario DATE NOT NULL,
+	CodUsuario INTEGER NOT NULL,
+    Denuncia INTEGER NOT NULL,
 	CONSTRAINT PK_Comentario PRIMARY KEY(Codigo,IdPostagem),
 	CONSTRAINT FK_Comentario FOREIGN KEY(IdPostagem) REFERENCES Postagem(Id)
-		ON UPDATE CASCADE ON DELETE RESTRICT
+		ON UPDATE CASCADE ON DELETE RESTRICT,
+	CONSTRAINT FK_Usuario FOREIGN KEY(CodUsuario) REFERENCES Usuario(Codigo)
+	        ON UPDATE CASCADE ON DELETE RESTRICT
 );
+
 
 CREATE TABLE Estante(
 	Id SERIAL,
-	IdConteudo INTEGER,
 	DataCriacao DATE NOT NULL,
 	CodUsuario INTEGER,
 	CONSTRAINT PK_Estante PRIMARY KEY(Id),
 	CONSTRAINT FK_Usuario FOREIGN KEY(CodUsuario) REFERENCES Usuario(Codigo)
-		ON UPDATE CASCADE ON DELETE RESTRICT,
-	CONSTRAINT FK_Conteudo FOREIGN KEY(Idconteudo) REFERENCES Conteudo(Id)
 		ON UPDATE CASCADE ON DELETE RESTRICT
+
 );
 
 CREATE TABLE Prateleira(
-	Codigo SERIAL, 
+	Codigo SERIAL,
 	IdEstante INTEGER,
 	Descricao VARCHAR(500) NOT NULL,
 	DataCriacao DATE NOT NULL,
@@ -86,7 +85,9 @@ CREATE TABLE Prateleira(
 	CONSTRAINT PK_Prateleira PRIMARY KEY(Codigo, IdEstante),
 	CONSTRAINT FK_Prateleira FOREIGN KEY(IdEstante) REFERENCES Estante(Id)
 		ON UPDATE CASCADE ON DELETE RESTRICT
+
 );
+
 
 
 CREATE TABLE Conteudo(
@@ -95,7 +96,7 @@ CREATE TABLE Conteudo(
 	Local VARCHAR(100) NOT NULL,
 	IdPostagem INTEGER,
 	CONSTRAINT PK_Conteudo PRIMARY KEY(Id),
-	CONSTRAINT FK_Conteudo FOREIGN KEY(IdPostagem) REFERENCES Postagem(Id)
+	CONSTRAINT FK_Postagem FOREIGN KEY(IdPostagem) REFERENCES Postagem(Id)
 		ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
@@ -110,10 +111,23 @@ CREATE TABLE AvaliaConteudo(
 		ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
+CREATE TABLE PrateleiraConteudo(
+  CodPrateleira INTEGER,
+  IdEstante INTEGER,
+  CodConteudo INTEGER,
+  CONSTRAINT PK_PrateleiraConteudo PRIMARY KEY(CodPrateleira, CodConteudo),
+  CONSTRAINT FK1_PrateleiraConteudo FOREIGN KEY(CodPrateleira,IdEstante) REFERENCES Prateleira(Codigo,IdEstante)
+		ON UPDATE CASCADE ON DELETE RESTRICT,
+  CONSTRAINT FK2_PrateleiraConteudo FOREIGN KEY(CodConteudo) REFERENCES Conteudo(Id)
+		ON UPDATE CASCADE ON DELETE RESTRICT
+);
 
+CREATE TABLE Alerta(
+   Id SERIAL,
+   Conteudo VARCHAR(1000),
+   CodUsuario INTEGER,
+   CONSTRAINT PK_Alerta PRIMARY KEY(Id),
+   CONSTRAINT FK_Alerta FOREIGN KEY(CodUsuario) REFERENCES Usuario(Codigo)
+		ON UPDATE CASCADE ON DELETE RESTRICT
 
-
-
-
-	
-	
+)
