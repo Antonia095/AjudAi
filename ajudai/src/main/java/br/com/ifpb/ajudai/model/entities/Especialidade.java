439f322b8a87fc94629d3e4dab273a72dfc6b79d
James Amarante <amarante189@gmail.com>
2019-12-09 10:01:29 -0300

    Criando filtro de acesso e modelando algumas classes com agregação

---
+++ ajudai/src/main/java/br/com/ifpb/ajudai/model/entities/Especialidade.java
@@ -0,0 +1,20 @@
+package br.com.ifpb.ajudai.model.entities;
+
+public class Especialidade {
+    private String area;
+
+    public Especialidade() {
+    }
+
+    public Especialidade(String area) {
+        this.area = area;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+}
